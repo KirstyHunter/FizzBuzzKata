2050f679e5d354ced7273f647cf4c141303e59d9
Kirsty Hunter <kirst2k@hotmail.com>
2016-01-26 20:21:31 +0000

    Created FizzBuzz Kata project

---
+++ FizzBuzz.Tests/UnitTest1.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace FizzBuzz.Tests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void WhenNumberIsOneReturnOne()
+        {
+            var fizzBuzz = new FizzBuzz();
+            var result = fizzBuzz.Calculate(1);
+            Assert.AreEqual("1", result);
+        } 
+    }
+}
